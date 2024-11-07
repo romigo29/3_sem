0861aac4fc711640cfcb78a22386912b04bb5449
Igor <152262528+romigo29@users.noreply.github.com>
2024-11-07 23:52:58 +0300

    Add files via upload

---
+++ АиСД/Лаб 6 алгортим Хаффмана/HuffmanAlgorithm/HuffmanAlgorithm/algorithm.cpp
@@ -0,0 +1,132 @@
+﻿#include <iostream>
+#include <queue>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <Windows.h>
+using namespace std;
+
+struct Node {
+    char ch;
+    int freq;
+    int ord;
+    Node* left;
+    Node* right;
+
+    Node(char character, int frequency, int order) {
+        ch = character;
+        freq = frequency;
+        ord = order;
+        left = right = nullptr;
+    }
+};
+
+struct Compare {
+    bool operator()(Node* l, Node* r) {
+
+        if (l->freq == r->freq) {
+            return l->ch < r->ch;
+        }
+
+        return l->freq > r->freq;
+    }
+};
+
+Node* buildHuffmanTree(unordered_map<char, int>& freqMap, unordered_map<char, int>& order) {
+
+    priority_queue<Node*, vector<Node*>, Compare> minHeap;
+
+    for (auto& pair : freqMap) {
+        char ch = pair.first;
+        int frequency = pair.second;
+        int index = order[ch]; 
+        minHeap.push(new Node(ch, frequency, index));
+    }
+
+    while (minHeap.size() > 1) {
+
+        Node* left = minHeap.top(); minHeap.pop();
+        Node* right = minHeap.top(); minHeap.pop();
+
+        Node* newNode = new Node('\0', left->freq + right->freq, max(left->ord, right->ord));
+        newNode->left = left;
+        newNode->right = right;
+
+        minHeap.push(newNode);
+    }
+
+    return minHeap.top();
+}
+
+void generateHuffmanCodes(Node* root, string str, unordered_map<char, string>& huffmanCode) {
+    if (!root) {
+        return;
+    }
+
+    if (!root->left && !root->right) {
+        huffmanCode[root->ch] = str;
+    }
+
+    generateHuffmanCodes(root->left, str + "0", huffmanCode);
+    generateHuffmanCodes(root->right, str + "1", huffmanCode);
+}
+
+void deleteTree(Node* root) {
+    if (root) {
+        deleteTree(root->left);
+        deleteTree(root->right);
+        delete root;
+    }
+}
+
+int main() {
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    string text;
+    cout << "Введите текст для кодирования: ";
+    getline(cin, text);
+    cout << "Исходная строка: " << text << endl;
+
+    // Шаг 1
+    unordered_map<char, int> freqMap;
+    unordered_map<char, int> order;
+    int ind = 0;
+    for (char ch : text) {
+
+        if (freqMap.find(ch) == freqMap.end()) {
+            order[ch] = ind++;
+        }
+
+        freqMap[ch]++;
+    }
+
+    cout << "Таблица встречаемости символов:\n";
+    for (const auto& pair : freqMap) {
+        cout << "'" << pair.first << "' встречается " << pair.second << " раз/а\n";
+    }
+
+    // Шаг 2
+    Node* root = buildHuffmanTree(freqMap, order);
+
+    // Шаг 3
+    unordered_map<char, string> huffmanCode;
+    generateHuffmanCodes(root, "", huffmanCode);
+
+    cout << "\nТаблица соответствия символов и кодов:\n";
+    for (auto& pair : huffmanCode) {
+        cout << "'" << pair.first << "' -> " << pair.second << "\n";
+    }
+
+    // Шаг 4
+    string encodedString = "";
+    for (char ch : text) {
+        encodedString += huffmanCode[ch];
+    }
+
+    cout << "\nВыходная последовательность:\n";
+    cout << encodedString << endl;
+
+    deleteTree(root);
+    return 0;
+}
---
+++ АиСД/Лаб 7 возрастающая подпоследовательность/IncreasingSubsequence/IncreasingSubsequence/algorithm.cpp
@@ -0,0 +1,87 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+void getLIS(vector<int>, int);
+
+static int checkData() {
+	int parm;
+	cin >> parm;
+	while (cin.fail()) {
+		cout << "Введено не число. Попробуйте еще раз" << endl;
+		//очистка консоли с ошибкой
+		cin.clear();
+		//игнориурем символы и переходим в конец потока
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cin >> parm;
+	}
+
+	return parm;
+}
+
+void getLIS(vector<int>seq, int N) {
+
+	vector<int> lengths(N, 1);
+	vector<int> previous(N, -1);
+
+	for (int i = 1; i < N; i++) {
+		for (int j = 0; j < i; j++) {
+			if (seq[j] < seq[i] && lengths[j] + 1 > lengths[i]) {
+				lengths[i] = lengths[j] + 1;
+				previous[i] = j;
+			}
+		}
+	}
+
+	int maxLength = 0, maxIndex = 0;
+	for (int i = 0; i < N; i++) {
+		if (lengths[i] > maxLength) {	
+			maxLength = lengths[i];
+			maxIndex = i;
+		}
+	}
+
+	vector<int> path;
+	while (maxIndex != -1) {
+		path.push_back(seq[maxIndex]);
+		maxIndex = previous[maxIndex];
+	}
+
+	reverse(path.begin(), path.end());
+
+	cout << "\nДлина наибольшей возрастающей подпоследовательности: " << maxLength << endl;
+	cout << "Наибольшая подпоследовательность: ";
+	for (int n : path) {
+		cout << n << " ";
+	}
+}
+int main() {
+
+	setlocale(LC_ALL, "rus");
+	srand(time(0));
+	int N;
+
+	cout << "Введите натуральное число (количество чисел в последовательности): ";
+	N = checkData();
+	while (N <= 0) {
+		cout << "Введено не натуральное число. Попробуйте еще раз: ";
+		N = checkData();
+	}
+
+	vector<int> sequence;
+	cout << "Введите последовательность чисел: ";
+	int num;
+	for (int i = 0; i < N; i++) {
+		cin >> num;
+		sequence.push_back(num);
+	}
+
+	cout << "Исходная последовательность: ";
+	for (int n : sequence) {
+		cout << n << " ";
+	}
+
+	cout << "\n------------------------------" << endl;
+	getLIS(sequence, N);
+	return 0;
+}
