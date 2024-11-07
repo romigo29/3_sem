b569e09052cbe3ef14018512ae1e6929d44603d0
Igor <152262528+romigo29@users.noreply.github.com>
2024-11-08 00:04:07 +0300

    Add files via upload

---
+++ КПО/Asm01/SE_Asm01/SE_Asm01/asm01.asm
@@ -0,0 +1,29 @@
+.586P													;система команд(процессор Pentium)
+.MODEL FLAT, STDCALL									;модель памяти, соглашение о вызовах
+includelib kernel132.lib								;компоновщику: компоновать с kernel132
+
+ExitProcess PROTO : DWORD								;прототип функции для завершения процесса
+MessageBoxA PROTO : DWORD, : DWORD, : DWORD, : DWORD	;прототип API-функции MessageBoxA 
+
+.STACK 4096								;выделение стека
+
+.CONST									;сегмент констант
+
+.DATA									;сегмент данных
+MB_OK	EQU 0							;EQU определяет константу
+STR1	DB "Моя первая программа", 0	;строка, первый элемент данных + нулевой бит
+STR2	DB "Привет, БГТУ!", 0
+HW		DD ?
+
+.CODE									;сегмент кода
+
+main PROC 								;точка входа main
+START:
+
+	INVOKE MessageBoxA, HW, OFFSET STR2, OFFSET STR1, MB_OK
+
+	push - 1							;код возврата (-1) процесса Windows(параметр ExitProcess)
+	call ExitProcess					;завершение процесса Windows
+main ENDP								;конец процедуры
+
+end main								;конец модуля main
