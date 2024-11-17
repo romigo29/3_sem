804ab1b80101f90ed74d091941ae1547720b0637
Igor <152262528+romigo29@users.noreply.github.com>
2024-11-17 22:24:45 +0300

    Add files via upload

---
+++ ООП/Лаб 10/LINQ/LINQ/Program.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			string[] Months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+
+			Console.Write("Введите число n - количесто букв в месяце: ");
+			int n = Convert.ToInt32(Console.ReadLine());
+			var MonthsSortedBySize = from m in Months
+									 where m.Length == n
+									 select m;
+
+			Console.Write($"Месяцы с числом букв {n}: ");
+			PrintCollection(MonthsSortedBySize);
+
+			var OnlyWinterSummerMonths = from m in Months
+										 where m.StartsWith('J') || m == "August" || m == "December" || m == "February"
+										 select m;
+
+			Console.Write("Только зиминие и летние месяцы: ");
+			PrintCollection(OnlyWinterSummerMonths);
+
+			var AlphabetSortedMonths = from m in Months
+									   orderby m.ToLower() 
+									   select m;
+
+			Console.Write("\nМесяцы в алфавитном порядке: ");
+			PrintCollection(AlphabetSortedMonths);
+
+			var MonthWithU = from m in Months
+							 where m.ToLower().Contains('u') && m.Length >= 4
+							 select m;
+
+			Console.Write("\nМесяцы длиной не менее 4, содержащие не менее 4 символов: ");
+			PrintCollection(MonthWithU);
+
+			SeparateParts();
+
+			Rectangle rectangle1 = Rectangle.CreateRectangle(2, 3, 2, 3, "rectangle");
+			Rectangle rectangle2 = Rectangle.CreateRectangle(5, 4, 5, 4, "rectangle");
+			Rectangle square1 = Rectangle.CreateRectangle(1, 1, 1, 1, "square");
+			Rectangle square2 = Rectangle.CreateRectangle(2, 2, 2, 2, "square");
+			Rectangle rhombus1 = Rectangle.CreateRectangle(3, 3, 3, 3, "rhombus");
+			Rectangle rhombus2 = Rectangle.CreateRectangle(4, 4, 4, 4, "rhombus");
+			Rectangle default1 = Rectangle.CreateRectangle(1, 3, 5, 7, "arbitrary");
+			Rectangle default2 = Rectangle.CreateRectangle(2, 4, 6, 8, "arbitrary");
+			Rectangle default3 = Rectangle.CreateRectangle(11, 9, 7, 5, "arbitrary");
+			Rectangle default4 = Rectangle.CreateRectangle(10, 8, 6, 4, "arbitrary");
+
+			List<Rectangle> rectangles = new List<Rectangle>
+			{
+				rectangle1, rectangle2, square1, square2, rhombus1, rhombus2, default1, default2, default3, default4
+			};
+
+
+			var DifferentRectanglesCount = rectangles
+				.GroupBy(r => r.GetObjType())
+				.Select(group => new {Type = group.Key, Count = group.Count() });
+
+			Console.WriteLine("\nКоличество различных типов фигур в коллекции:");
+			foreach (var group in DifferentRectanglesCount)
+			{
+				Console.WriteLine($"Тип: {group.Type}, Количество: {group.Count}");
+			}
+
+
+			var MinMaxRectanglesSquaresPerimeter = rectangles
+				.GroupBy(r => r.GetObjType())
+				.Select(group => new
+				{
+					Type = group.Key,
+					MinPerimeterFig = group.Aggregate((min, next) => next.GetPerimeter() < min.GetPerimeter() ? next : min),
+					MaxPerimeterFig = group.Aggregate((max, next) => next.GetPerimeter() > max.GetPerimeter() ? next : max),
+					MinSquareFig = group.Aggregate((min, next) => next.GetSquare() < min.GetSquare() ? next : min),
+					MaxSquareFig = group.Aggregate((max, next) => next.GetSquare() > max.GetSquare() ? next : max)
+				});
+
+			Console.WriteLine("\nМинимальные и максимальные периметры и площади: ");
+			foreach (var group in MinMaxRectanglesSquaresPerimeter)
+			{
+				Console.WriteLine($"Тип: {group.Type}," +
+				$" Минимальный периметр: {group.MinPerimeterFig.GetPerimeter()} " +
+				$" Максимальный периметр: {group.MaxPerimeterFig.GetPerimeter()}," +
+				$" Минимальная площадь: {group.MinSquareFig.GetSquare()}, " +
+				$" Максимальная площадь: {group.MaxSquareFig.GetSquare()}");
+			}
+
+
+			Console.WriteLine("\nВведите x (Для сравнения со стороной квадрата):");
+			int x = Convert.ToInt32(Console.ReadLine());
+
+			var SquaresLessThanX = rectangles
+				.Where(r => r.GetObjType() == "square" && r.GetSquareSideSize() < x)
+				.ToArray();
+
+			Console.WriteLine($"Квадраты со сторонами меньшими, чем заданнный x({x}):");
+			foreach (var square in SquaresLessThanX)
+			{
+				Console.WriteLine(square);
+			}
+
+
+			var SortedPerimeterRectangleArray = rectangles
+				.Where(r => r.GetObjType() == "rectangle")
+				.OrderBy(r => r.GetPerimeter())
+				.ToArray();
+
+			Console.WriteLine($"\nУпорядоченный по периметру прямоугольников массив:");
+			foreach (var rectangle in SortedPerimeterRectangleArray)
+			{
+				Console.WriteLine(rectangle);
+				Console.WriteLine($"Периметр {rectangle.GetPerimeter()}");
+			}
+
+			SeparateParts();
+
+			var MyQuery = rectangles
+				.Where(r => r.GetSquare() <= 10 && r.GetObjType() != "arbitrary")
+				.OrderBy(r => r.GetObjType())
+				.ThenBy(r => r.GetPerimeter())
+				.GroupBy(r => r.GetObjType())
+				.Select(group => new
+				{
+					Type = group.Key,
+					MinPerimeterFig = group.Min(r => r.GetPerimeter()),
+					MaxPerimeterFig = group.Max(r => r.GetPerimeter()),
+					Total = group.Count()
+				});
+
+			Console.WriteLine($"\nРезультат пользовательского запроса: ");
+			foreach(var group in  MyQuery)
+			{
+				Console.WriteLine($"Тип объекта: {group.Type}");
+				Console.WriteLine($"Минимальная площадь: {group.MinPerimeterFig}");
+				Console.WriteLine($"максимальная плоащдь: {group.MaxPerimeterFig}");
+				Console.WriteLine($"Общее количество: {group.Total}");
+
+			}
+
+			SeparateParts();
+
+			var GeneralInformation = DifferentRectanglesCount
+				.Join(
+					MinMaxRectanglesSquaresPerimeter,
+					counter => counter.Type,
+					parameteres => parameteres.Type,
+					(counter, parameters) => new
+					{
+						counter.Type,
+						Total = counter.Count,
+						MinPerimeter = parameters.MinPerimeterFig,
+						MaxPerimeter = parameters.MaxPerimeterFig,
+						MinSquare = parameters.MinSquareFig,
+						MaxSquare = parameters.MaxSquareFig,
+					}
+				);
+
+			Console.WriteLine("\nОбщая информация о фигурах в коллекции: ");
+			foreach (var group in GeneralInformation)
+			{
+				Console.WriteLine($"\nТип: {group.Type}");
+				Console.WriteLine($"Количество: {group.Total}");
+				Console.WriteLine($"Минимальный периметр: {group.MinPerimeter.GetPerimeter()}");
+				Console.WriteLine($"Максимальный периметр: {group.MaxPerimeter.GetPerimeter()}");
+				Console.WriteLine($"Максимальная площадь: {group.MinSquare.GetSquare()}");
+				Console.WriteLine($"Максимальная площадь: {group.MaxSquare.GetSquare()}");
+			}
+
+		}
+
+		public static void PrintCollection(IEnumerable array)
+		{
+			foreach (var element in array)
+			{
+				Console.Write($"{element} ");
+			}
+		}
+
+		public static void SeparateParts()
+		{
+			Console.WriteLine("\n---------------------------------------------------------");
+		}
+	}
+}
---
+++ ООП/Лаб 10/LINQ/LINQ/Rectangle.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+	partial class Rectangle
+	{
+		private int height1, height2, width1, width2;
+		static int objectAmount;
+		private readonly string objType;
+		private readonly Guid ID;
+
+		static Rectangle()
+		{
+			objectAmount = 0;
+		}
+
+		public Rectangle()
+		{
+			objType = "square";
+			height1 = height2 = width1 = width2 = 1;
+
+			ID = Guid.NewGuid();
+			objectAmount++;
+		}
+		private Rectangle(int height1, int height2, int width1, int width2, string objType)
+		{
+			this.height1 = height1;
+			this.height2 = height2;
+			this.width1 = width1;
+			this.width2 = width2;
+			this.objType = objType;
+
+			ID = Guid.NewGuid();
+			objectAmount++;
+		}
+		public Rectangle(string objType = "arbitrary", int height1 = 3, int height2 = 3, int width1 = 3, int width2 = 4)
+		{
+			this.objType = objType;
+			this.height1 = height1;
+			this.height2 = height2;
+			this.width1 = width1;
+			this.width2 = width2;
+
+			ID = Guid.NewGuid();
+			objectAmount++;
+		}
+		public static Rectangle CreateRectangle(int height1, int width1, int height2, int width2, string objType)
+		{
+			return new Rectangle(height1, height2, width1, width2, objType);
+		}
+		public double GetSquare()
+		{
+			switch (this.objType)
+			{
+				case "rectangle":
+					return this.height1 * this.width1;
+
+				case "square":
+					return this.height1 * this.height1;
+
+				case "rhombus":
+					return (this.height1 * this.height1) / 2;
+
+				default:
+					double p = (this.height1 + this.width1 + this.height2 + this.width2) / 2;
+					return Math.Sqrt((p - this.height1) * (p - this.width1) * (p - this.height2) * (p - this.width2));
+			}
+		}
+		public int GetPerimeter()
+		{
+			return this.height1 + this.height2 + this.width1 + this.width2;
+		}
+
+		public string GetObjType()
+		{
+			return this.objType;
+		}
+
+		public int GetSquareSideSize()
+		{
+			return height1;
+		}
+
+		public static int GetAmount()
+		{
+			return objectAmount;
+		}
+
+		public static void OutputInfo(int n1, int n2, int n3, int n4)
+		{
+			Console.WriteLine($"Количество объектов, находящихся в классе: {objectAmount}");
+			Console.WriteLine($"Количество прямоугольников: {n1}");
+			Console.WriteLine($"Количество квадратов: {n2}");
+			Console.WriteLine($"Количество ромбов: {n3}");
+			Console.WriteLine($"Количество произвольных четырехугольников: {n4}");
+		}
+
+		public static void GetMaxMin(Rectangle[] rectangles, string type)
+		{
+			double minSquare = double.MaxValue; double maxSquare = double.MinValue;
+			int minPerimeter = int.MaxValue; int maxPerimeter = int.MinValue;
+
+			foreach (var obj in rectangles)
+			{
+
+				string objType = obj.objType;
+
+				if (objType == type)
+				{
+					double square = obj.GetSquare();
+					int perimeter = obj.GetPerimeter();
+
+					if (square > maxSquare)
+					{
+						maxSquare = square;
+					}
+
+					if (square < minSquare)
+					{
+						minSquare = square;
+					}
+
+					if (perimeter < minPerimeter)
+					{
+						minPerimeter = perimeter;
+					}
+
+					if (perimeter > maxPerimeter)
+					{
+						maxPerimeter = perimeter;
+					}
+				}
+			}
+
+			if (maxSquare != double.MinValue && minSquare != double.MaxValue)
+			{
+				Console.WriteLine($"\nТип: {type}");
+				Console.WriteLine($"Наибольшая площадь: {maxSquare}");
+				Console.WriteLine($"Наименьшая площадь: {minSquare}");
+				Console.WriteLine($"Наибольший периметр: {maxPerimeter}");
+				Console.WriteLine($"Наименьший периметр: {minPerimeter}");
+			}
+		}
+
+		public int SetHeight1
+		{
+			private set
+			{
+				if (value < 1)
+				{
+					Console.WriteLine("Число должно быть положительным");
+				}
+				else
+				{
+					height1 = value;
+				}
+			}
+
+			get
+			{
+				return height1;
+			}
+		}
+
+		public int SetHeight2
+		{
+			private set
+			{
+				if (value < 1)
+				{
+					Console.WriteLine("Число должно быть положительным");
+				}
+				else
+				{
+					height2 = value;
+				}
+			}
+
+			get
+			{
+				return height2;
+			}
+		}
+
+		public int SetWidth1
+		{
+			private set
+			{
+				if (value < 1)
+				{
+					Console.WriteLine("Число должно быть положительным");
+				}
+				else
+				{
+					width1 = value;
+				}
+			}
+
+			get
+			{
+				return width1;
+			}
+		}
+
+		public int SetWidth2
+		{
+			private set
+			{
+				if (value < 1)
+				{
+					Console.WriteLine("Число должно быть положительным");
+				}
+				else
+				{
+					width2 = value;
+				}
+			}
+
+			get
+			{
+				return width2;
+			}
+		}
+	}
+
+	partial class Rectangle
+	{
+		public override bool Equals(object obj)
+		{
+			if (obj == null || this.GetType() != obj.GetType())
+				return false;
+
+			Rectangle other = (Rectangle)obj;
+
+			return this.height1 == other.height1 &&
+				   this.width1 == other.width1 &&
+				   this.objType == other.objType;
+		}
+
+		public override int GetHashCode()
+		{
+			int hash = 17;
+			hash = hash * 31 + height1.GetHashCode();
+			hash = hash * 31 + width1.GetHashCode();
+			hash = hash * 31 + (objType != null ? objType.GetHashCode() : 0);
+			return hash;
+		}
+
+		public override string ToString()
+		{
+			return $"Фигура: Height1 = {height1}, Width1 = {width1}, Type = {objType}";
+		}
+	}
+}
